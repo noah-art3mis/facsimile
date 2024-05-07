d0f829a63e4586eeb7af0201658c5fb0969438b1
Gustavo Araujo Costa <gustavo@arcos.org.br>
2024-05-07 17:41:07 -0300

    add scale factor to resize pages

--- README.md
+++ README.md
@@ -16,6 +16,8 @@ Possible names:
 
 Generates simulacrum plates using HTML and CSS using [html2canvas](https://html2canvas.hertzen.com/). Part of AUTOMATON, the simulacrum automation package.
 
+-   [ ] https://gist.github.com/ricardoleme/8a4578493cf345d3fea381c796007f08
+
 ## How to
 
 1.  get a book file. a book file is a json file in the following format:
@@ -34,6 +36,7 @@ Generates simulacrum plates using HTML and CSS using [html2canvas](https://html2
     -   https://color.adobe.com/create/image-gradient
     -   https://color.adobe.com/create/image
     -   https://cssgradient.io/
+1.  `npm run dev` to start the server and then open the browser
 1.  upload the file using the interface.
 1.  wait for it to process all pages
 1.  press `download all`
--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,10 @@ document.addEventListener('DOMContentLoaded', () => {
         getBook(e);
     });
 
+    // document
+    //     .getElementById('btn-expand')
+    //     ?.addEventListener('click', expandPlates);
+
     document
         .getElementById('btn-preview')
         ?.addEventListener('click', compilePlates);
@@ -18,149 +22,202 @@ document.addEventListener('DOMContentLoaded', () => {
     document
         .getElementById('btn-download')
         ?.addEventListener('click', () => downloadAllZip());
-});
 
-function getBook(event: Event) {
-    const files = (event.target as HTMLInputElement)?.files;
-    if (files && files.length > 0) {
-        const file: File = files[0];
-        const reader = new FileReader();
-        reader.readAsText(file); // triggers 'onload' event and sets 'result' attribute
+    document
+        .getElementById('btn-next')
+        ?.addEventListener('click', () => nextPage);
 
-        reader.onload = function (e) {
-            const bookStr = e.target?.result;
-            const bookObj: Book = JSON.parse(bookStr as string);
-            localStorage.setItem('bookId', bookObj.id);
-            generatePlates(bookObj);
-        };
-    }
-}
+    function getBook(event: Event) {
+        const files = (event.target as HTMLInputElement)?.files;
+        if (files && files.length > 0) {
+            const file: File = files[0];
+            const reader = new FileReader();
+            reader.readAsText(file); // triggers 'onload' event and sets 'result' attribute
 
-function generatePlates(book: Book) {
-    validateData(book);
-    console.log(book.pages[0].content);
-    rechunkSentences(book, MAX_LENGTH_CONTENT);
-    console.log(book.pages[0].content);
-    populateOriginals(book);
-}
+            reader.onload = function (e) {
+                const bookStr = e.target?.result;
+                const bookObj: Book = JSON.parse(bookStr as string);
+                sessionStorage.setItem('bookId', bookObj.id);
+                sessionStorage.setItem('book', JSON.stringify(bookObj));
+                sessionStorage.setItem(
+                    'bookPages',
+                    bookObj.pages.length.toString()
+                );
+                setNumberOfPages();
 
-function populateOriginals(book: Book) {
-    const container = document.createElement('div');
-    container.classList.add('original-pages');
-    document.querySelector('body')?.appendChild(container);
-
-    for (let i = 0; i < book.pages.length; i++) {
-        const page = document.createElement('div');
-        page.classList.add('page');
-        document.querySelector('.original-pages')?.appendChild(page);
-        page.id = book.id + '-' + book.pages[i].number;
-
-        for (let j = 0; j < book.pages[i].content.length; j++) {
-            const plate = createPlate(book, book.pages[i], j);
-            page.appendChild(plate);
-        }
-    }
-}
-
-function createPlate(book: Book, page: Page, index: number) {
-    const plate = document.createElement('div');
-    plate.classList.add('plate');
-    plate.id = book.id + '-' + page.number + '-' + index;
-
-    const content = document.createElement('p');
-    content.textContent = page.content[index];
-    content.classList.add('content');
-    plate.appendChild(content);
-
-    const reference = document.createElement('div');
-    reference.classList.add('reference');
-    plate.appendChild(reference);
-
-    const author = document.createElement('p');
-    author.textContent = book.author;
-    author.classList.add('author');
-    reference.appendChild(author);
-
-    const title = document.createElement('p');
-    title.textContent = book.title;
-    title.classList.add('title');
-    reference.appendChild(title);
-
-    return plate;
-}
-
-function compilePlates() {
-    const container = document.createElement('div');
-    container.classList.add('preview-pages');
-    document.querySelector('body')?.appendChild(container);
-
-    const pages = document.querySelector('.original-pages');
-
-    for (const page of pages!.children) {
-        const _page = document.createElement('div');
-        _page.classList.add('preview-page');
-        container.appendChild(_page);
-
-        for (const plate of page.children) {
-            html2canvas(plate as HTMLElement).then(
-                (canvas: HTMLCanvasElement) => {
-                    const src = canvas.toDataURL(`image/${IMAGE_TYPE}`, 1.0);
-                    const div = createPreview(src, plate.id);
-                    _page.appendChild(div);
-                    // @ts-ignore
-                    plate.style.display = 'none';
-                }
-            );
+                generatePlates(bookObj);
+            };
         }
     }
 
-    // const btnPreview = document.getElementById(
-    //     'btn-preview'
-    // ) as HTMLButtonElement;
-    // btnPreview.disabled = true;
-    const btnDownload = document.getElementById(
-        'btn-download'
-    ) as HTMLButtonElement;
-    btnDownload.disabled = false;
-}
+    function setNumberOfPages() {
+        const totalPagesElement = document.querySelector('.total-pages');
+        if (totalPagesElement) {
+            const pages = sessionStorage.getItem('bookPages');
+            totalPagesElement.textContent = pages;
+        }
+    }
 
-function createPreview(src: string, name: string) {
-    const container = document.createElement('div');
-    container.classList.add('preview');
+    function generatePlates(book: Book) {
+        validateData(book);
+        rechunkSentences(book, MAX_LENGTH_CONTENT);
+        populateOriginals(book);
+    }
 
-    const a = document.createElement('a');
-    a.href = src;
-    a.download = `${name}.${IMAGE_TYPE}`;
-    container.appendChild(a);
+    function populateOriginals(book: Book) {
+        const container = document.createElement('div');
+        container.classList.add('small-page-container');
+        document.querySelector('body')?.appendChild(container);
 
-    const img = document.createElement('img');
-    img.src = src;
-    img.style.width = 1080 / PREVIEW_SIZE + 'px';
-    img.style.height = 1350 / PREVIEW_SIZE + 'px';
-    a.appendChild(img);
+        for (let i = 0; i < book.pages.length; i++) {
+            const page = document.createElement('div');
+            page.classList.add('page');
+            page.classList.add('hidden-page');
+            document.querySelector('.small-page-container')?.appendChild(page);
+            page.id = book.id + '-' + book.pages[i].number;
 
-    return container;
-}
-
-async function downloadAllZip() {
-    const links = document.querySelectorAll('.preview a');
-    const files = await Promise.all(
-        Array.from(links).map(async (link: Element) => {
-            const name = link.getAttribute('download');
-            if (link instanceof HTMLAnchorElement) {
-                const response = await fetch(link.href);
-                const blob = await response.blob();
-                return { name: name, input: blob };
+            for (let j = 0; j < book.pages[i].content.length; j++) {
+                const plate = createPlate(book, book.pages[i], j);
+                page.appendChild(plate);
             }
-            return null;
-        })
-    );
+        }
+    }
 
-    // @ts-ignore
-    const content = await downloadZip(files).blob();
-    const fileName = localStorage.getItem('bookId');
-    FileSaver.saveAs(
-        content,
-        `${fileName ? fileName : 'semblance-results'}.zip`
-    );
-}
+    function createPlate(book: Book, page: Page, index: number) {
+        const plate = document.createElement('div');
+        plate.classList.add('plate');
+        plate.id = book.id + '-' + page.number + '-' + index;
+
+        const plateBlock = document.createElement('div');
+        plateBlock.classList.add('plate-block');
+        plate.appendChild(plateBlock);
+
+        const content = document.createElement('p');
+        content.textContent = page.content[index];
+        content.classList.add('content');
+        plateBlock.appendChild(content);
+
+        const reference = document.createElement('div');
+        reference.classList.add('reference');
+        plateBlock.appendChild(reference);
+
+        const author = document.createElement('p');
+        author.textContent = book.author;
+        author.classList.add('author');
+        reference.appendChild(author);
+
+        const title = document.createElement('p');
+        title.textContent = book.title;
+        title.classList.add('title');
+        reference.appendChild(title);
+
+        return plate;
+    }
+
+    function expandPlates() {
+        document.documentElement.style.setProperty('--scale', '5');
+    }
+
+    function allPlatesVisible() {
+        document.querySelectorAll('.page').forEach((page) => {
+            if (page.classList.contains('hidden-page')) {
+                page.classList.remove('hidden-page');
+            }
+        });
+    }
+
+    function compilePlates() {
+        allPlatesVisible();
+        expandPlates();
+
+        const container = document.createElement('div');
+        container.classList.add('summary-pages');
+        document.querySelector('body')?.appendChild(container);
+
+        const pages = document.querySelector('.small-page-container');
+
+        for (const page of pages!.children) {
+            const _page = document.createElement('div');
+            _page.classList.add('summary-page');
+            container.appendChild(_page);
+
+            for (const plate of page.children) {
+                html2canvas(plate as HTMLElement).then(
+                    (canvas: HTMLCanvasElement) => {
+                        const src = canvas.toDataURL(
+                            `image/${IMAGE_TYPE}`,
+                            1.0
+                        );
+                        const div = createPreview(src, plate.id);
+                        _page.appendChild(div);
+                        // @ts-ignore
+                        plate.style.display = 'none';
+                    }
+                );
+            }
+        }
+
+        // const btnPreview = document.getElementById(
+        //     'btn-preview'
+        // ) as HTMLButtonElement;
+        // btnPreview.disabled = true;
+        const btnDownload = document.getElementById(
+            'btn-download'
+        ) as HTMLButtonElement;
+        btnDownload.disabled = false;
+    }
+
+    function createPreview(src: string, name: string) {
+        const container = document.createElement('div');
+        container.classList.add('preview');
+
+        const a = document.createElement('a');
+        a.href = src;
+        a.download = `${name}.${IMAGE_TYPE}`;
+        container.appendChild(a);
+
+        const img = document.createElement('img');
+        img.src = src;
+        img.style.width = 1080 / PREVIEW_SIZE + 'px';
+        img.style.height = 1350 / PREVIEW_SIZE + 'px';
+        a.appendChild(img);
+
+        return container;
+    }
+
+    function nextPage() {
+        // const currentPage = document.querySelector('.current-page');
+        // if (currentPage) {
+        //     currentPage.remove();
+        // }
+        // let n = 1;
+        // const parent = document.querySelector('.parent'); // Select the parent element
+        // const child = parent?.querySelector(`.child:nth-child(${n})`); // Select the nth child
+        // if (child) {
+        //     child.classList.remove('special'); // Remove the 'special' class
+        // }
+    }
+
+    async function downloadAllZip() {
+        const links = document.querySelectorAll('.preview a');
+        const files = await Promise.all(
+            Array.from(links).map(async (link: Element) => {
+                const name = link.getAttribute('download');
+                if (link instanceof HTMLAnchorElement) {
+                    const response = await fetch(link.href);
+                    const blob = await response.blob();
+                    return { name: name, input: blob };
+                }
+                return null;
+            })
+        );
+
+        // @ts-ignore
+        const content = await downloadZip(files).blob();
+        const fileName = sessionStorage.getItem('bookId');
+        FileSaver.saveAs(
+            content,
+            `${fileName ? fileName : 'semblance-results'}.zip`
+        );
+    }
+});
