cb3a7b80c3ef080b03e6592e2994dcaee8c5529b
Gustavo Araujo Costa <gustavo@arcos.org.br>
2024-06-29 17:10:46 -0300

    remove old validation

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -2,9 +2,6 @@ import { Book } from './types.ts';
 
 export function validateData(data: Book) {
     isBook(data);
-    // arePagesUnique(data);
-    // endsWithPunctuation(data);
-    // notTooLong(data);
 }
 
 function isBook(data: Book) {
@@ -26,55 +23,5 @@ function isBook(data: Book) {
         const message = `Book pages are missing`;
         alert(message);
         console.log(message);
-    }
-}
-
-function arePagesUnique(data: Book) {
-    const pageNumbers = new Set<string>();
-    for (const page of data.pages) {
-        if (pageNumbers.has(page.number)) {
-            const message = `Page number ${page.number} is not unique`;
-            console.log(message);
-        }
-        pageNumbers.add(page.number);
-    }
-}
-
-function endsWithPunctuation(data: Book) {
-    for (const page of data.pages) {
-        for (const plate of page.content) {
-            if (plate.endsWith('.')) {
-                continue;
-            }
-
-            if (plate.endsWith('?')) {
-                continue;
-            }
-
-            if (plate.endsWith('!')) {
-                continue;
-            }
-
-            if (plate.endsWith('"')) {
-                continue;
-            }
-
-            if (plate.endsWith("'")) {
-                continue;
-            }
-
-            const message = `${page.number} should end with punctuation`;
-            console.log(message);
-        }
-    }
-}
-
-function notTooLong(data: Book) {
-    for (const page of data.pages) {
-        if (page.content.length > 10) {
-            const message = `${page.number} has more than 10 sentences`;
-            alert(message);
-            console.log(message);
-        }
     }
 }
